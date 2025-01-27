573cb35099a5d93ad4a3e052a41de6cdf6ca921a
Timo Ziegler <ziegler-timo@outlook.com>
2025-01-27 10:51:14 +0100

    Added copying of vpn script

--- fresh.sh
+++ fresh.sh
@@ -5,13 +5,15 @@ echo "Setting up your Mac..."
 # Check whether .oh-my-zsh file is in home directory and install otherwise.
 # Do not run zsh when installation has finished
 if ! [ -e $HOME/.oh-my-zsh ]; then
+  echo 'Installing Oh-my-zsh...'
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)" bash --unattended
 else
-  echo 'Oh my zsh already installed'
+  echo 'Oh-my-zsh already installed'
 fi
  
 # Check for Homebrew and install if we don't have it
 if test ! $(command -v brew); then
+  echo 'Installing Homebrew...'
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
@@ -67,6 +69,10 @@ fi
 if ! [ -e $HOME/Private ]; then
   mkdir $HOME/Private
 fi
+
+# Copy vpn scripts to usr/local/bin
+echo 'Copying vpn scripts...'
+sudo cp vpn-LUH.sh usr/local/bin
 
 # Symlink the Mackup config file to the home directory
 # ln -s ./.mackup.cfg $HOME/.mackup.cfg
