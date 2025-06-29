8124bf1cbe8aa09cb819b930f8e7e8bc540ef46b
Timo Ziegler <ziegler-timo@outlook.com>
2025-06-29 14:49:45 +0200

    Updated tex.sh to link texmf from within .dotfiles

--- tex.sh
+++ tex.sh
@@ -3,6 +3,12 @@
 TEXMF_DIR=$HOME/Library/texmf
 DOTFILES_DIR=$HOME/.dotfiles
 
+# Install all LaTex dependencies using tlmgr from newline delimited list Texfile
+echo "Installing LaTex dependencies..."
+sudo tlmgr update --self
+sudo tlmgr install $(grep -v '^\s*#' Texfile | sed 's/#.*//' | tr -s '' | tr "\n" " ")
+echo "LaTex dependencies installed..."
+
 #Create a symbolic from $HOME/Library/texmf to the texmf file provided in the dotfiles
 if [ ! -L "$TEXMF_DIR" ]; then
   echo "Creating symbolic link from $TEXMF_DIR ..."
