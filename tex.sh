367113910305cf417841e67f59945e23bafe0499
Timo Ziegler <timo@mac.speedport.ip>
2025-06-29 14:46:36 +0200

    Updated tex.sh to create pointer to texmf within .dotfiles

--- tex.sh
+++ tex.sh
@@ -3,12 +3,19 @@
 TEXMF_DIR=$HOME/Library/texmf
 DOTFILES_DIR=$HOME/.dotfiles
 
-#Create a symbolic from $HOME/Library/texmf to the texmf file provided in the dotfiles
+# Create a symbolic from $HOME/Library/texmf to the texmf file provided in the dotfiles
 if [ ! -L "$TEXMF_DIR" ]; then
   echo "Creating symbolic link from $TEXMF_DIR ..."
   ln -s $DOTFILES_DIR/texmf $TEXMF_DIR
 else
   echo "Symbolic link from $TEXMF_DIR already exists"
 fi
+
+# Update TeX file database; you might need to enter your admin password
+sudo mktexlsr
+
+# Install all LaTex dependencies using tlmgr from newline delimited list Texfile
+sudo tlmgr update --self
+sudo tlmgr install $(grep -v '^\s*#' Texfile | sed 's/#.*//' | tr -s '' | tr "\n" " ")
 
 echo "TeX Setup complete"
