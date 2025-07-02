4795479ba0e9a961bb6b181d6ebce5e819251443
Timo Ziegler <timo@mac.speedport.ip>
2025-07-02 09:22:04 +0200

    Added TeXShop engine to compile temporary files into /build directory of the project which gets symlinked

--- tex.sh
+++ tex.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-TEXMF_DIR=$HOME/Library/texmf
 DOTFILES_DIR=$HOME/.dotfiles
+ENGINE_DIR=$HOME/Library/TeXShop/Engines
+TEXMF_DIR=$HOME/Library/texmf
 
 # Install all LaTex dependencies using tlmgr from newline delimited list Texfile
 echo "Installing LaTex dependencies..."
 sudo tlmgr update --self
 sudo tlmgr install $(grep -v '^\s*#' Texfile | sed 's/#.*//' | tr -s '' | tr "\n" " ")
-echo "LaTex dependencies installed..."
+echo "LaTex dependencies installed."
 
-#Create a symbolic link from texmf in dotfiles to $HOME/Library/texmf
+# Create a symbolic link from pdfLaTeXWithBuild.engine to TeXShop engines
+echo "Symlinking TeXShop engine..."
+chmod +x $DOTFILES_DIR/pdfLaTeXWithBuild.engine
+mkdir -p $ENGINE_DIR
+ln -sf "$DOTFILES_DIR/pdfLaTeXWithBuild.engine" "$ENGINE_DIR/pdfLaTeXWithBuild.engine"
+echo "Symlink to $DOTFILES_DIR/pdfLaTeXWithBuild.engine created."
+
+# Create a symbolic link from texmf in dotfiles to $HOME/Library/texmf
 if [ ! -L "$TEXMF_DIR" ]; then
-  echo "Creating symbolic link from $TEXMF_DIR ..."
-  ln -s $DOTFILES_DIR/texmf $TEXMF_DIR
+  echo "Symlinking to $DOTFILES_DIR/texmf..."
+  ln -sf $DOTFILES_DIR/texmf $TEXMF_DIR
+  echo "Symlink to $DOTFILES_DIR/texmf created."
 else
-  echo "Symbolic link from $TEXMF_DIR already exists"
+  echo "Symlink to $DOTFILES_DIR/texmf already exists."
 fi
 
 # Update TeX file database; you might need to enter your admin password
